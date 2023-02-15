631674a6ce0660b4c1472ac3463bbb90f5a52ad8
Hitodama0 <biagior00@gmail.com>
2023-02-15 12:35:09 +0100

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Biagio Ricci on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
